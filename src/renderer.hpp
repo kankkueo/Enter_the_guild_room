32cd3f7592f95d0fc5c536c8b6a501efcdb019a4
Elmo Kankkunen <elmo.kankkunen@aalto.fi>
2023-11-13 18:00:51 +0200

    reder flags in class

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -19,12 +19,15 @@ public:
     SDL_Texture* loadTexture(const char*);
     void drawTexture(SDL_Texture*, int, int);
     void destroy();
+    void set_flags(uint32_t, uint32_t);
 
 
 private:
 
     SDL_Renderer* renderer_;
 	SDL_Window* window_;
+    uint32_t renderer_flags_;
+    uint32_t window_flags_;
 
 };
 
