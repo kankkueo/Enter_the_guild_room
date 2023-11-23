16d2770acc64f8b52e702c9f7ff37c6983185f5d
Johannes <johannes.viirimaki@gmail.com>
2023-11-23 13:15:26 +0200

    add method for creating mobs

--- src/room.cpp
+++ src/room.cpp
@@ -1,5 +1,11 @@
 #include "room.hpp"
 
 Room::Room(std::string name, int width, int height, SDL_Texture *texture, SDL_Texture *advanceDoor, int advanceDoorX, int advanceDoorY)
-    : name_(name), width_(width), height_(height), texture_(texture), advanceDoor_(advanceDoor), advanceDoorX_(advanceDoorX), advanceDoorY_(advanceDoorY) { }
+    : name_(name), width_(width), height_(height), texture_(texture), advanceDoor_(advanceDoor), advanceDoorX_(advanceDoorX), advanceDoorY_(advanceDoorY) {
+        monsters_;
+     }
 
+void Room::addMonster() {
+    Monster monster1 = Monster(50, 5, 4, 300, 300, 20, 20); // randomize these?
+    monsters_.push_back(monster1);
+}
--- src/room.hpp
+++ src/room.hpp
@@ -11,6 +11,7 @@ class Room {
 public:
     Room(std::string name, int width, int height, SDL_Texture *texture, SDL_Texture *advanceDoor, int advanceDoorX, int advanceDoorY);
 
+    void addMonster();
     
     std::string name_;
     SDL_Texture *texture_;
