4778184b75ab80eef8383152ec403f56b3734262
Elmo Kankkunen <elmo.kankkunen@aalto.fi>
2023-12-10 21:10:42 +0200

    updated docs

--- doc/html/search/all_8.js
+++ doc/html/search/all_8.js
@@ -1,7 +1,10 @@
 var searchData=
 [
   ['meleemob_0',['MeleeMob',['../classMeleeMob.html',1,'']]],
-  ['monster_1',['Monster',['../classMonster.html',1,'']]],
-  ['movemonsters_2',['moveMonsters',['../classGame.html#a7fd6e655e1c839db99962737b0a0316b',1,'Game']]],
-  ['moveprojectiles_3',['moveProjectiles',['../classGame.html#af0688eea131b45ca074b5e60cff334c5',1,'Game']]]
+  ['menurender_1',['menuRender',['../classGame.html#ae0f89f04b52d3f9c825bd6a7d2cd5898',1,'Game']]],
+  ['menuselect_2',['menuSelect',['../classGame.html#a9868a4834e953a00f3ccf1500dde4c1d',1,'Game']]],
+  ['menutick_3',['menuTick',['../classGame.html#a7cd6af30c03814d92c5ec57492bd4590',1,'Game']]],
+  ['monster_4',['Monster',['../classMonster.html',1,'']]],
+  ['movemonsters_5',['moveMonsters',['../classGame.html#a7fd6e655e1c839db99962737b0a0316b',1,'Game']]],
+  ['moveprojectiles_6',['moveProjectiles',['../classGame.html#af0688eea131b45ca074b5e60cff334c5',1,'Game']]]
 ];
--- doc/html/search/functions_4.js
+++ doc/html/search/functions_4.js
@@ -1,5 +1,8 @@
 var searchData=
 [
-  ['movemonsters_0',['moveMonsters',['../classGame.html#a7fd6e655e1c839db99962737b0a0316b',1,'Game']]],
-  ['moveprojectiles_1',['moveProjectiles',['../classGame.html#af0688eea131b45ca074b5e60cff334c5',1,'Game']]]
+  ['menurender_0',['menuRender',['../classGame.html#ae0f89f04b52d3f9c825bd6a7d2cd5898',1,'Game']]],
+  ['menuselect_1',['menuSelect',['../classGame.html#a9868a4834e953a00f3ccf1500dde4c1d',1,'Game']]],
+  ['menutick_2',['menuTick',['../classGame.html#a7cd6af30c03814d92c5ec57492bd4590',1,'Game']]],
+  ['movemonsters_3',['moveMonsters',['../classGame.html#a7fd6e655e1c839db99962737b0a0316b',1,'Game']]],
+  ['moveprojectiles_4',['moveProjectiles',['../classGame.html#af0688eea131b45ca074b5e60cff334c5',1,'Game']]]
 ];
